fb704a240aa22d37e25bf3f90ca3bc4f3d681fc4
Abdussalam <mr.aljbri@qq.com>
2021-11-20 21:31:12 +0800

    Update AssemblyInfo.cs

--- zatca/Properties/AssemblyInfo.cs
+++ zatca/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("zatca")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("dot Net Library that implements the ZATCA (Zakat Tax and Customs Authority) for e-invoicing requirements in Saudi Arabia")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("zatca")]
+[assembly: AssemblyCompany("ALJBRI")]
+[assembly: AssemblyProduct("Zatca.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
